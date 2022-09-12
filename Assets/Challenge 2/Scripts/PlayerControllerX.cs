ce43990699d328d4898c2df0f9c7470e5696b4a0
iramovchan <86199439+iramovchan@users.noreply.github.com>
2022-09-12 15:24:33 +0200

    First commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    float timer = 1;
+    bool timerreached = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // wait 1 second before you are able to create dog again
+        if (!timerreached)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer > 1)
+        {
+            timerreached = true;
+        }
+
+        // On spacebar press, send dog
+        if (timerreached)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                timerreached = false;
+                timer = 0;
+            }
+        }
+    }
+}
